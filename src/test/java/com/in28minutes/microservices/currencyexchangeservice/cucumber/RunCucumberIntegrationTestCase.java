45900d0c93fc31cf933b2db68b0d9c88462272a5
Victor Montoya <vmontoyadelosrios@gmail.com>
2025-06-25 17:23:20 -0600

    Elimine los test de integracion, de momento me voy a saltar eso

--- src/test/java/com/in28minutes/microservices/currencyexchangeservice/cucumber/RunCucumberIntegrationTestCase.java
+++
@@ -1,9 +0,0 @@
-package com.in28minutes.microservices.currencyexchangeservice.cucumber;
-
-import io.cucumber.junit.platform.engine.Cucumber;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-@Cucumber
-public class RunCucumberIntegrationTestCase {
-}
